aa2319435f172773987b63ee60ce533089a143f6
Theo Steiner <theosteiner@gmail.com>
2022-04-20 18:16:20 +0900

    Extract default mapping so it can be modified/extended

---
+++ lua/warped/default_mapping.lua
@@ -0,0 +1,27 @@
+return {
+	background = "background",
+	foreground = "foreground",
+	-- text color
+	white = "normal_white",
+	black = "normal_black",
+	-- responsible for errors, lua table keys, gitsigns delete
+	red = "normal_red",
+	-- Responsible for: git signs added, lua strings
+	green = "bright_green",
+	-- Responisble for <Component> / <html-tag> inner color gitsigns modified, lualine normal mode
+	yellow = "normal_blue",
+	-- TODO: Swap this to a better color (non-blue) and customize only in dracula to be like this
+	-- selected color, link inside of a tag, "local" declaration
+	blue = "normal_cyan",
+	-- neo-tree folder color
+	orange = "normal_yellow",
+	aqua = "bright_blue",
+	cyan = "normal_green",
+	purple = "normal_magenta",
+	violet = "bright_cyan",
+	brown = "normal_white",
+	seagreen = "bright_red",
+	turquoise = "bright_magenta",
+	-- seems unimportant
+	pink = "bright_yellow",
+}
--- lua/warped/init.lua
+++ lua/warped/init.lua
@@ -2,33 +2,6 @@ local Warped = {}
 local utils = require("warped.utils")
 Warped.colorbuddy = utils.try_require("colorbuddy")
 
-local default_mapping = {
-	background = "background",
-	foreground = "foreground",
-	-- text color
-	white = "normal_white",
-	black = "normal_black",
-	-- responsible for errors, lua table keys, gitsigns delete
-	red = "normal_red",
-	-- Responsible for: git signs added, lua strings
-	green = "bright_green",
-	-- Responisble for <Component> / <html-tag> inner color gitsigns modified, lualine normal mode
-	yellow = "normal_blue",
-	-- selected color, link inside of a tag, "local" declaration
-	blue = "normal_cyan",
-	-- neo-tree folder color
-	orange = "normal_yellow",
-	aqua = "bright_blue",
-	cyan = "normal_green",
-	purple = "normal_magenta",
-	violet = "bright_cyan",
-	brown = "normal_white",
-	seagreen = "bright_red",
-	turquoise = "bright_magenta",
-	-- seems unimportant
-	pink = "bright_yellow",
-}
-
 -- map vim colors to 16 terminal colors
 local adapt_colorscheme = function(_, theme_colors, mapping)
 	if theme_colors then
@@ -43,7 +16,7 @@ function Warped.setup(config)
 	-- set defaults for settings if undefined
 	config = config or {}
 	config.onchange_callback = config.onchange_callback or adapt_colorscheme
-	config.color_mapping = config.color_mapping or default_mapping
+	config.color_mapping = config.color_mapping or require("warped.default_mapping")
 	config.theme_config = config.theme_config or require("warped.default_theme_config")
 
 	-- setup colorbuddy if available
