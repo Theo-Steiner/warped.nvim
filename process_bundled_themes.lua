229bdb467ca5018fb184c7a6be055f2745b387f8
Theo Steiner <theosteiner@gmail.com>
2022-04-23 10:45:28 +0900

    Add script to generate default themes from local copy of warpdotdev/themes

--- process_bundled_themes.lua
+++ process_bundled_themes.lua
@@ -1,2 +1,7 @@
 local processing_utils = dofile("./lua/warped/processing.lua")
-processing_utils.generate_theme_module("./themes/warp_bundled", "./lua/warped/default_themes/")
+-- Warp's bundled themes for now are just formatted as lower case,
+-- without the underscore present in custom themes, so we'll have to remove it manually here.
+local process_output_name = function(output_name)
+	return output_name:gsub("_", "")
+end
+processing_utils.generate_theme_module("./themes/warp_bundled", "./lua/warped/default_themes/", process_output_name)
