76ef0129fa113504dd4cd88794955f50640d3f6b
Theo Steiner <theosteiner@gmail.com>
2022-05-24 16:08:24 +0900

    Fit error caused by recursive calling out of scope

--- lua/warped/file_watching.lua
+++ lua/warped/file_watching.lua
@@ -9,21 +9,25 @@ local handle_err = function(err, fs_event)
 end
 
 -- initiate a file watcher that executes a callback when the specified file is changed
-function M.initiate(file_path, on_change_callback)
+function Initiate(file_path, on_change_callback)
 	local file_watcher = vim.loop.new_fs_event()
 	---@diagnostic disable-next-line: unused-local
-	local callback = vim.schedule_wrap(function(err, fname, events)
+	local callback = function(err, fname, events)
 		if err then
 			handle_err(err, file_watcher)
 		else
 			-- For some reason the callback is not called repeatedly, unless I detach and reattach the watcher.
 			file_watcher:stop()
 			on_change_callback()
-			M.initiate(file_path, on_change_callback)
+			Initiate(file_path, on_change_callback)
 		end
-	end)
+	end
 	-- attach file watcher
-	file_watcher:start(file_path, {}, callback)
+	file_watcher:start(file_path, {}, function()
+		vim.schedule(callback)
+	end)
 end
+
+M.initiate = Initiate
 
 return M
